422940c8c9ebf8a8be85cefa8a103fdb698684aa
lebrancconvas <lebrancconvas@gmail.com>
2022-11-04 21:01:51 +0700

    ✨ feat: add primitive type lab

---
+++ lab_primitive_type/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let num01: u8 = 100;
+    // let num02 = 200;
+    println!("{}", num01);
+    println!("{}", num01);
+
+    let num01: u8 = 10 + num01;
+    println!("{}", num01);
+
+    let num01: u8 = 20 + num01;
+    println!("{}", num01);
+
+    let bool1: bool = true;
+    let bool2: bool = false;
+    println!("{}", bool1);
+    println!("{}", bool2);
+}
